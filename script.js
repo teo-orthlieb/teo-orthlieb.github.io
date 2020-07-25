1935fe75b3ee14cc8b373c6f1a75324646415cd4
Teo Orthlieb <inspirateur.pro@gmail.com>
2020-07-25 14:53:14 -0400

    basic tabs

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function on_load() {
+    let tab = new URLSearchParams(window.location.search).get("t");
+    openTab(tab);
+}
+
+
+function openTab(button, tab) {
+    // Get all elements with class="tabcontent" and hide them
+    let tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    let tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tab).style.display = "block";
+    button.className += " active";
+}
