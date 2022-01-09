e48e3988148a191d7219a3c18fc01c59a91eef49
Teo Orthlieb <teo.orthlieb@gmail.com>
2022-01-09 11:34:49 +0100

    added language color

--- load_repos.mjs
+++ load_repos.mjs
@@ -5,6 +5,7 @@ const starIcon = `<svg aria-label="stars" role="img" height="16" viewBox="0 0 16
 const forkIcon = `<svg aria-label="forks" role="img" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-repo-forked">
 <path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
 </svg>`;
+const colors = await (await fetch("colors.json")).json();
 
 function htmlToElement(html) {
 	var template = document.createElement('template');
@@ -49,9 +50,13 @@ async function repo_card(owner, repo) {
 	// lang
 	let language = resp["data"]["language"];
 	if (language !== null) {
-		let lang = document.createElement("small")
+		let lang_color = document.createElement("span");
+		lang_color.classList.add("language_color");
+		lang_color.style.backgroundColor = colors[language];
+		let lang = document.createElement("small");
 		lang.style.marginRight = "2em";
 		lang.innerText = language;
+		stats.appendChild(lang_color);
 		stats.appendChild(lang);
 	}
 	// stars
