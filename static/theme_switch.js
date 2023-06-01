471a6ecdcde7669d09e61c056cca4597061a80e3
Teo <teo.orthlieb@gmail.com>
2023-06-01 19:06:24 +0200

    fixed color theme getting forgotten between pages

--- static/theme_switch.js
+++ static/theme_switch.js
@@ -1,12 +1,31 @@
+if (get_theme() === "light") {
+    set_light();
+} else {
+    set_dark();
+}
+document.body.setAttribute("class", "");
+
+
+function get_theme() {
+    return localStorage.getItem("theme") || "dark";
+}
+
+function set_light() {
+    document.getElementById("theme_toggle").setAttribute("src", "/sun_icon.png");
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+}
+
+function set_dark() {
+    document.getElementById("theme_toggle").setAttribute("src", "/moon_icon.png");
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+}
+
 function on_theme_switch() {
-    console.log("hello");
-    let is_dark = document.body.classList.contains("dark");
-    let toggle = document.getElementById("theme_toggle");
-    if (is_dark) {
-        document.body.setAttribute("class", "");
-        toggle.setAttribute("src", "/sun_icon.png");
+    if (get_theme() === "dark") {
+        set_light();
     } else {
-        toggle.setAttribute("src", "/moon_icon.png");
-        document.body.setAttribute("class", "dark");
+        set_dark();
     }
 }
